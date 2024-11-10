5bb155d5644084d14ab79373a9d388983a16215b
Ruslan Kabalin <ruslan.kabalin@gmail.com>
2024-11-10 19:19:36 +0000

    Version and minimum requirement bump (3.10+)

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022032900;
-$plugin->requires  = 2016052300; // Moodle 3.1+.
+$plugin->version   = 2024111000;
+$plugin->requires  = 2020110911; // Moodle 3.10+.
 $plugin->component = 'local_courseduplication';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.2.3';
+$plugin->release   = '3.2.4';
